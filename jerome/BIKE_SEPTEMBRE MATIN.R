b2a6f8319699b9c67803259b51622bc6ad9c5aed
over-jay <lepagnol@hotmail.com>
2016-12-30 13:02:29 +0100

    via l'import de tout le mois de septembre

--- jerome/BIKE_SEPTEMBRE MATIN.R
+++ jerome/BIKE_SEPTEMBRE MATIN.R
@@ -1,6 +1,8 @@
 #Chargement des données
+# fichier des trajets
+# on va prendre le 22/9/2016 and plus du 15/9/2016
 
-BIKE <- read.csv('./jerome/20160915-matin.csv', sep=";", head=TRUE, as.is=TRUE)
+BIKE <- read.csv('./jerome/201609-citibike-tripdata.csv', sep=",", head=TRUE, as.is=TRUE)
 
 head(BIKE,5)
 dim(BIKE)
@@ -52,10 +54,9 @@ BIKE$DURATION<-BIKE$DTTM_ARR-BIKE$DTTM_DEP
 ########################################################################
 # Compter le nb de départs par station
 ########################################################################
-
-install.packages("data.table")
+if (!("data.table" %in% rownames(installed.packages()))) install.packages("data.table")
 library("data.table")
-install.packages('dplyr')
+if (!("dplyr" %in% rownames(installed.packages()))) install.packages("dplyr")
 library('dplyr')
 
 tableau<-table(BIKE$start.station.id,BIKE$end.station.id) 
@@ -65,7 +66,8 @@ tableau<-table(BIKE$start.station.id,BIKE$end.station.id)
 #  tail(tableau)
 
 vecteur<-tableau
-dim(vecteur)<- c( 577*571,1)
+dim(tableau)[1] *   dim(tableau)[2]
+dim(vecteur)<- c( dim(tableau)[1] *   dim(tableau)[2],1)
  # vecteur
  # str(vecteur)
 vecteur<-as.numeric(vecteur)
@@ -99,17 +101,236 @@ trajetall <- rbind(trajet,trajet2)
 
 # quelles station on le plus de deport + arrivée
   trajetall<-trajetall[,-2,]
-summary(trajetall)
+# summary(trajetall)
 
 trajetsum<- aggregate(x= trajetall$n, by= list((trajetall$start.station.id))
                                              , FUN = sum)  
 
 # pair de trajets classés par nombre de transport A ou R
 trajetsum<- trajetsum [rev(order(trajetsum$x)),]
-# plus que 200 par exemple (il y en a 50)
-trajetsum[trajetsum$x> 200,1]
-# donc 50 stations qui on plus de 200 depart et arrivées
-tops<-trajetsum[trajetsum$x> 200,1]
+# plus que 10.000 / 200 par exemple (il y en a 107 / 50)
+trajetsum[trajetsum$x> 10000,1]
+# donc 107 / 50 stations qui on plus de 10.000  200 depart et arrivées
+tops<-trajetsum[trajetsum$x> 10000,1]
 
+#CJ.table.2 <- function(X,Y) {
+#  eval(parse(text=paste0("setkey(X[,c(k=1,.SD)],k)[Y[,c(k=1,.SD)],list(",paste0(unique(c(names(X),names(Y))),collapse=","),")][,k:=NULL]")))
+#}
+
+BK1<-data.table(BIKE)
+BK2<-data.table(BIKE)
+#setkey 
+#res<-setkey(BK1[,c(k=1,.SD)],k)[BK2[,c(k=1,.SD)],allow.cartesian=TRUE][,k:=NULL]
+#BK1[BK2, on=c("birth.year","usertype","gender")]
+
+
+rm(BK1)
+rm(BK2)
+rm(BKtemp1)
+rm(BKtemp2)
+rm(BKtemp)
+rm(BU)
+rm(BKsymo)
+rm(BKsymoT)
+rm(ssi)
+
+#du lundi 19 au 23 septembre
+BK1<-BIKE[  (strftime(BIKE$DTTM_DEP, format = "%e") == 20)  ,]
+ind<- ( is.na(BK1$start.station.id)  |   BK1$start.station.id == "NA"  )
+BK1 <-  BK1[!(ind),]
+BK2 <-  BK1
+BU<-BK1[,c("start.station.id","end.station.id")]
+ind<- ( is.na(BU$start.station.id)  |   BU$start.station.id == "NA"  )
+BU <-  BU[!(ind),]
+BU<-unique(BU)
+
+
+
+BKsym<-NULL
+BKlen<-NULL
+
+for ( i in 1:length(BU[,1]) )
+{
+  ssi<-BU[i,1]
+  esi<-BU[i,2]
+  if (ssi>esi)
+    #on va ramasser les trajets de ssi à esi et de esi à ssi => pas besoin de prendre esi>ssi
+  {
+    #print (ssi, esi)
+    #print (paste(i," - ", ssi, esi))
+    symuser<-intersect(
+      BK1[BK1$start.station.id==ssi & BK1$end.station.id==esi, 
+          c("gender","birth.year","usertype") ] 
+      ,
+      BK2[BK2$start.station.id==esi & BK2$end.station.id==ssi, 
+          c("gender","birth.year","usertype") ])
+    if (length(symuser[,1]) !=0 & esi!=ssi ) {
+      # ici on a ssi = start et esi = end avec une personne similaire (yob/gender/type)
+      # pour chacun, il faudrait identifier le gender/yob/type
+      # print (i)
+      for (j in 1:length(symuser[,1]))
+      {gender<-symuser[j,1]
+      birth.year<-symuser[j,2]
+      usertype<-symuser[j,3]
+      # trajet de ssi à esi
+      BKtemp1<- BK1[BK1$start.station.id==ssi 
+                     & BK1$end.station.id == esi
+                     & BK1$gender == gender
+                     & BK1$usertype == usertype
+                     & BK1$birth.year == birth.year,]
+      # trajet de esi à ssi
+      BKtemp2<- BK1[BK1$start.station.id==esi 
+                     & BK1$end.station.id == ssi
+                     & BK1$gender == gender
+                     & BK1$usertype == usertype
+                     & BK1$birth.year == birth.year,]
+      names(BKtemp2)<-paste("2.",names(BKtemp2), sep="")
+      
+      
+      # traiter le cas où on a 2 trajet retour ou 1 aller
+     BKtemp<-merge(BKtemp1, BKtemp2, all = TRUE)
+     # BKtemp<-cbind(BKtemp1[1,], BKtemp2[1,])
+      BKtlen<-cbind(ssi,esi, usertype, gender, birth.year,length(BKtemp[,1]), length(BKtemp[,2]))
+      if (length(BKtemp[,1])* length(BKtemp[,2])>1) BKlen <- rbind(BKlen, BKtlen)
+      BKsym <- rbind(BKsym,(BKtemp))
+      }
+    }
+  }
+}
+
+#?order
+BKsymo<-BKsym[order(BKsym$gender, BKsym$birth.year, BKsym$usertype),]
+  
+#il y a des lignes N/A a la fin ... a étudier
+BKsymo[,c("start.station.id", "end.station.id", "birth.year", "usertype", "gender", "DURATION") ]
+ind<- ( is.na(BKsymo$start.station.id)  |   BKsymo$start.station.id == "NA"  )
+BKsymo <-  BKsymo[!(ind),]
+
+BKsymo[,c("start.station.id", "end.station.id", "birth.year", "usertype", "gender", "DURATION") ]
+
+# 632*2 trajets symétriques - ou plus ... 
+
+# avoir une durée minimale entre aller et retour
+ind <- ( is.na(BKsymo[,"DTTM_DEP"]) | is.na(BKsymo[,"2.DTTM_DEP"]) ) 
+sum(ind)
+BKsymo <-  BKsymo[!(ind),]
+worktime<- 3*60 #minutes
+worktraj<-BKsymo[(BKsymo[,"DTTM_DEP"]- BKsymo[,"2.DTTM_DEP"]) > worktime,]
+
+worktraj[,c("start.station.id", "end.station.id", "birth.year", "usertype", "gender", "DURATION", "2.DURATION") ]
+
+
+# quel est le top 10 des stations in/out ?
+# top 10 des stations de départ- DPLYR
+worktraj %>% group_by (start.station.id) %>% summarize(n=n()) %>% arrange (desc(n)) %>% print
+# top 10 des stations d'arrivée - DPLYR
+worktraj %>% group_by (end.station.id) %>% summarize(n=n()) %>% arrange (desc(n)) %>% print
+plot(  worktraj$start.station.id  ,   worktraj$end.station.id , col = worktraj$gender)
+#  interessant = 0-500 parle à 0-500 puis 2000 puis 3000-3500
+
+names(worktraj)[c(34,36)]<-c('DTTM_DEP2', 'DURATION2')
+worktrajcount<-worktraj %>% group_by (start.station.id, start.station.name,start.station.latitude,start.station.longitude, end.station.id,
+                       end.station.name,end.station.latitude,end.station.longitude,usertype,birth.year,gender,DTTM_DEP,
+                       DURATION,DTTM_DEP2,DURATION2)%>% summarize(n=n()) %>% arrange (desc(n)) %>% print
+
+worktrajcount[, c("start.station.id", "end.station.id", "birth.year", "usertype", "gender", "DURATION", "DURATION2","n")]
+
+worktrajcount$start.station.id<-as.factor(x=worktrajcount$start.station.id)
+worktrajcount$end.station.id<-as.factor(x=worktrajcount$end.station.id)
+worktrajcount$usertype<-as.factor(x=worktrajcount$usertype)
+worktrajcount$gender<-as.factor(x=worktrajcount$gender)
+worktrajcount$DURATION<-as.numeric(x=worktrajcount$DURATION)
+worktrajcount$DURATION2<-as.numeric(x=worktrajcount$DURATION2)
+
+summary (worktrajcount)
+plot(worktrajcount$birth.year, worktrajcount$DURATION)
+points(worktrajcount$birth.year, worktrajcount$DURATION2, pch = 16, col = 2)
+points(worktrajcount$birth.year, worktrajcount$DURATION, pch = 17, col = 1)
+
+plot(worktrajcount$gender, worktrajcount$DURATION)
+plot(worktrajcount$gender, worktrajcount$DURATION2)
+
+plot(worktrajcount$start.station.id, worktrajcount$DURATION)
+plot(worktrajcount$end.station.id, worktrajcount$DURATION2)
+
+measure <-function (lat1, lon1, lat2, lon2)
+{  # generally used geo measurement function
+  R <- 6378.137; # Radius of earth in KM
+  dLat <- lat2 * pi / 180 - lat1 * pi / 180;
+  dLon <- lon2 * pi / 180 - lon1 * pi / 180;
+  a <- sin(dLat/2) * sin(dLat/2) + cos(lat1 * pi / 180) * cos(lat2 * pi / 180) * sin(dLon/2) * sin(dLon/2);
+  c <- 2 * atan2(sqrt(a), sqrt(1-a));
+  d <- R * c;
+  return (d * 1000) # meters
+}
+
+# source : http://stackoverflow.com/questions/639695/how-to-convert-latitude-or-longitude-to-meters
+
+worktrajcount$measure <- measure(worktrajcount$start.station.latitude, 
+                                 worktrajcount$start.station.longitude,
+                                 worktrajcount$end.station.latitude,
+                                 worktrajcount$end.station.longitude)
+worktrajcount$distL1<-abs (worktrajcount$start.station.latitude-worktrajcount$end.station.latitude) + abs(worktrajcount$start.station.longitude-worktrajcount$end.station.longitude)
+worktrajcount$distL2<-sqrt((worktrajcount$start.station.latitude-worktrajcount$end.station.latitude)^2 + (worktrajcount$start.station.longitude-worktrajcount$end.station.longitude)^2)
+
+# travail sur les vitesses comparées par genre en L1
+plot (worktrajcount$distL1 , worktrajcount$DURATION, pch = 20, cex = 0.3, col = worktrajcount$gender)
+mod<-lm(worktrajcount$DURATION ~worktrajcount$distL1 )
+abline(mod)
+mod1 <- lm(    (worktrajcount$DURATION[worktrajcount$gender==1])   ~ (worktrajcount$distL1[worktrajcount$gender ==1])  )
+abline(mod1, col = 2)
+mod2 <- lm(    (worktrajcount$DURATION[worktrajcount$gender==2])   ~ (worktrajcount$distL1[worktrajcount$gender ==2])  )
+abline(mod2, col = 3)
+mod1
+mod2
+
+summary(worktrajcount)
+# travail sur les vitesses comparées par genre en L2
+plot (worktrajcount$distL2 , worktrajcount$DURATION, pch = 20, cex = 0.3, col = worktrajcount$gender)
+mod<-lm(worktrajcount$DURATION ~worktrajcount$distL2 )
+abline(mod)
+mod1 <- lm(    (worktrajcount$DURATION[worktrajcount$gender==1])   ~ (worktrajcount$distL2[worktrajcount$gender ==1])  )
+abline(mod1, col = 2)
+mod2 <- lm(    (worktrajcount$DURATION[worktrajcount$gender==2])   ~ (worktrajcount$distL2[worktrajcount$gender ==2])  )
+abline(mod2, col = 3)
+mod1
+mod2
+summary(worktrajcount)
+
+
+# travail sur les vitesses comparées par genre en MEASURE
+plot (worktrajcount$measure , worktrajcount$DURATION, pch = 20, cex = 0.3, col = worktrajcount$gender)
+mod<-lm(worktrajcount$DURATION ~worktrajcount$measure )
+abline(mod)
+mod1 <- lm(    (worktrajcount$DURATION[worktrajcount$gender==1])   ~ (worktrajcount$measure[worktrajcount$gender ==1])  )
+abline(mod1, col = 2)
+mod2 <- lm(    (worktrajcount$DURATION[worktrajcount$gender==2])   ~ (worktrajcount$measure[worktrajcount$gender ==2])  )
+abline(mod2, col = 3)
+#vistesse moyenne homme  en metre / minute
+1/mod1[[1]][2]
+#vistesse moyenne femme en metre / minute
+1/mod2[[1]][2]
+
+
+
+# L1 et L2 sont proche, prennons measure à la place (qui a la bon gout de donner des metres)
+worktrajcount$deltadist <- worktrajcount$distL1 / worktrajcount$distL2
+library(rgl)
+plot3d (worktrajcount$distL1 , worktrajcount$distL2, worktrajcount$measure, col = worktrajcount$deltadist/5000)
+
+plot(worktrajcount$d,worktrajcount$distL2  )
+summary(worktrajcount)
+
+# calcul de la vitesse "speed"
+worktrajcount$speed <- worktrajcount$measure/worktrajcount$DURATION
+worktrajcount$speed2 <- worktrajcount$measure/worktrajcount$DURATION2
+                                   
+plot(worktrajcount$speed ~ worktrajcount$speed2, col = worktrajcount$gender)                                   
+mod<-lm( worktrajcount$speed2 ~ worktrajcount$speed  )                                   
+abline(mod)
+plot3d(worktrajcount$speed , worktrajcount$DURATION, worktrajcount$birth.year, col = as.numeric(worktrajcount$gender)+1)
+plot(worktrajcount$speed ~ worktrajcount$birth.year, col = worktrajcount$gender)                                   
+mod<-lm( worktrajcount$speed ~ worktrajcount$birth.year   )                                   
+abline(mod)
 
 
